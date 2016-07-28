4b8480b1b575d6e41d8aae14f06b283e2d648b21
Rossano <rossano.pantaleoni@gmail.com>
2016-07-29 01:37:27 +0200

    Rev. 1.0.4: Added dayDisplay.php + tweaks

--- php/refresh.php
+++ php/refresh.php
@@ -1,31 +1,32 @@
 <?php
 
-	include ($login.php);
+	include ('./login.php');
 	
 	// Connect to the database
-	$con = mysql_connect($host, $user, $passwd);
-	
-	if($con) {
-		die ('Could not connect: ' . mysql_error());
-	}
+	$con = mysql_connect($host, $user, $passwd) or die ('Could not connect: ' . mysql_error());
+	//if($con) {
+	//	die ('Could not connect: ' . mysql_error());
+	//}
 	
 	// Create the SQL query command
 	$sql = "select MAX(id) from " . $table;
 	
 	// Select the database
-	mysql_select_db($database);
+	mysql_select_db($database) or die ('Could not select!');
 	
 	// Make the sql query
-	$ret = mysql_query($sql, $con);
-	if(! $ret) {
-		die ('Could not get data: ' . mysql_error());
-	}
+	$result = mysql_query($sql, $con) or die ('Could not get data: ' . mysql_error());
+	//if(! $ret) {
+	//	die ('Could not get data: ' . mysql_error());
+	//}
 	
 	// Consume the result of the query
-	while($row = mysql_fetch_assoc($ret)) {
-		echo "Temperatura [°C]: " . $row['tTempData'];
-		echo "Umidità [%]: " . $row['tHumData'];
-	}
+	if ($result) {
+		while($row = mysql_fetch_assoc($ret)) {
+			echo "Temperatura [°C]: " . $row['tTempData'];
+			echo "Umidità [%]: " . $row['tHumData'];
+		}	
+	}	
 	
 	mysql_close($con);
 ?>
